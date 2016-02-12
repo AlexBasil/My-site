a3a6fe72b4a93695e053e5320f1913f16cde45ec
Vasiliy Aleksandrov <vasiliy.aleksandrov15@gmail.com>
2016-02-12 12:21:40 +0300

    finish without php

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,45 @@
-$(document).ready(function() {
- 	console.log('я на странице');
-});
+
+function openPopup() {
+	var win_width = $ ( window ).width()/2 - $('#popup').width()/2;
+	 $('#popup').bPopup({
+            follow: [false, false], //x, y
+            position: [win_width, 50] //x, y
+        });
+};
+
+var elem = $('form').find('input, textarea, .download_field').not('input[type="submit"], input[type="reset"]');
+
+function errorInput() {
+
+	if ($.trim(elem.val()).length > 0) {
+
+	} else {
+		elem.addClass('error');
+		elem.siblings('.tooltip').css('display','block');
+	}
+};
+
+
+function cancel_error() {
+	$(document.activeElement).removeClass('error');
+	$(document.activeElement).siblings('.tooltip').css('display','none');
+};
+	
+
+function cancel_file() {
+	$('.download_field').removeClass('error');
+	$('.download_field').siblings('.tooltip').css('display','none');
+};
+
+
+function full_reset() {
+	elem.removeClass('error');
+	elem.siblings('.tooltip').css('display','none');
+};
+
+function changeAdress() {
+
+	if ($('#upload').val().length > 0) {
+		$('.text_file').text($('#upload').val());
+	}
+}
